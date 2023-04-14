f67ff4cfc7c9dac8c501807b4ea0f4457a69b27b
Jacob Justice <jacobhjustice.dev@gmail.com>
2023-04-14 15:10:18 -0500

    WS-2160 (#3)
    * testing
    
    * fix
    
    * wip
    
    * '
    
    * shell
    
    * wip
    
    * test
    
    * wip
    
    * fix
    
    * test
    
    * owner
    
    * commit
    
    * revise
    
    * ls
    
    * redir
    
    * .
    
    * .
    
    * .
    
    * fix
    
    * .
    
    * dir
    
    * h
    
    * BRANCH
    
    * .
    
    * fatal
    
    * .
    
    * tag
    
    * test
    
    * .
    
    * .
    
    * .
    
    * .
    
    * fix
    
    * prev vers
    
    * test
    
    * status
    
    * depth
    
    * default vers
    
    * fix
    
    * var
    
    * testing w/ main
    
    * outputs
    
    * "
    
    * fix
    
    * _
    
    * input
    
    * wip
    
    * use branch name
    
    * allow updates
    
    * user prerelease
    
    * //
    
    * replace build
    
    * remove build
    
    * test build
    
    * fix output
    
    * use prerelease
    
    * ready for review

---
+++ workflows/scripts/compute-semver.py
@@ -0,0 +1,53 @@
+from distutils.log import fatal
+import sys
+from datetime import datetime
+import semver
+import re
+
+if (len(sys.argv) == 4):
+    previous_version = sys.argv[1]
+    if (previous_version == None):
+        previous_version = '1.0.0'
+    print("Previous Version: " + previous_version)
+
+    branch_name = sys.argv[2]
+    if (branch_name == ''):
+        fatal('Error: branch_name must not be empty')
+        sys.exit(1)
+
+    print("Branch Name: " + branch_name)
+
+    commit_body = sys.argv[3]
+    if (commit_body == ''):
+        print('Error: commit_body should not be empty. Will assume change is a PATCH')
+    else:
+        print("Commit Body: " + commit_body)
+
+    
+
+    if (re.search("[A-Za-z]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]{40}", previous_version) != None):
+        previous_version = "1.0.0"
+
+    ver = semver.Version.parse(semver.replace(str(previous_version), build=None))
+
+    if (branch_name == 'main'):
+        if (commit_body.find("[x] Major") != -1 or commit_body.find("[x ] Major") != -1 or commit_body.find("[ x] Major") != -1):
+            ver = ver.bump_major()
+        elif (commit_body.find("[x] Minor") != -1 or commit_body.find("[x ] Minor") != -1 or commit_body.find("[ x] Minor") != -1):
+            ver = ver.bump_minor()
+        else:
+            ver = ver.bump_patch()
+    else: # note: Using prerelease instead of build parameter of semver due to lack of docker support for "+" in image name
+        build = ver.prerelease
+        if (build == None or build.find(branch_name+".") == -1):
+            ver = semver.replace(str(ver), prerelease=branch_name+"."+str("1"))
+        else:
+            ver = ver.bump_prerelease()
+
+    print(ver)
+    print(f"::set-output name=version::" + str(ver))
+
+else: 
+    fatal('Error: compute-semver.py requires 3 arguments to be passed (previous_version, branch_name, commit_body)')
+    sys.exit(1)
+
